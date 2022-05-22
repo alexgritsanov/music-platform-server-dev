2f98cd8734c5821a547ae3a4bad1379e84f821ab
alex <agritsanov@list.ru>
2022-05-22 04:37:47 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from "@nestjs/common";
+
+import { TrackModule } from "./track/track.module";
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from "./file/file.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from 'path'
+
+
+@Module({
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve(__dirname, 'static')
+        }),
+        MongooseModule.forRoot('mongodb+srv://admin:1234@cluster0.z1uwp.mongodb.net/?retryWrites=true&w=majority'),
+        TrackModule,
+        FileModule
+    ],
+    // providers: [AppService]
+})
+export class AppModule { }
